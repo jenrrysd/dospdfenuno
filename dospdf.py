2362cd8b5736811feb8829eaee6c73d117b943ac
dextre <dextre1481@gmail.com>
2023-05-19 08:25:04 +0000

    reeplanteado casi tod el codigo

--- dospdf.py
+++ dospdf.py
@@ -1,6 +1,10 @@
 from PyPDF2 import PdfMerger
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import messagebox
+import subprocess
+import os
+import shutil
 
 # Función para seleccionar los archivos PDF
 def select_files():
@@ -19,12 +23,32 @@ merger = PdfMerger()
 for pdf_file in pdf_files:
     merger.append(pdf_file)
 
-# Solicitar al usuario el nombre del archivo resultante
-output_file = input("Ingresa el nombre del archivo resultante (sin la extensión .pdf): ")
+# Obtenemos el nombre del usario para ponerlo en el directorio Documentos
+username = os.getenv('USER')
+documentos_dir = f"/home/{username}/Documentos"
 
-# Combinar los archivos PDF en uno solo
-merger.write(f"{output_file}.pdf")
-merger.close()
+try:
+    command = ["zenity", "--entry", "--title", "Juntar dos PDF en uno", "--text", "Ingresa un nombre para el archivo resultante (sin la extensión .pdf)"]
+    output_file = subprocess.check_output(command).decode("utf-8").strip()
 
-print(f"Los archivos PDF se han combinado en {output_file}.pdf correctamente.")
+    # Combinar los archivos PDF en uno solo
+    merger.write(f"{output_file}.pdf")
+    merger.close()
 
+    # Mensaje de la conbiancion creada.
+    messagebox.showinfo("Combinación de archivos PDF", f"El nuevo nombre del PDF es:\n{output_file}.pdf \nEl pdf esta ubicado en:\n{documentos_dir}")
+
+    # Despues de crear el nuevo pdf lo mueve al directorio Documentos
+    shutil.move(f"{output_file}.pdf", os.path.join(documentos_dir, f"{output_file}.pdf"))
+
+
+except subprocess.CalledProcessError as e:
+    if e.returncode == 1:
+        # El usuario hizo clic en "Cancelar" en el cuadro de diálogo de Zenity
+        messagebox.showinfo("Combinación de archivos PDF", "Operación cancelada por el usuario")
+    else:
+        # Otro error de subprocess, muestra el mensaje de error
+        messagebox.showerror("Error", str(e))
+
+##Creado por Jenrry Soto Dextre, correo dextre1481@gmail.com , jsd@dextre.xyz
+##este script en python te aseguro qeu corre en fedora Gnu/Linux
